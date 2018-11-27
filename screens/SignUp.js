1a3559d51dc3dd6bcf90824117246857b4677e80
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-11-28 04:35:52 +0900

    ScrollViewでラップ（Android対応）

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button, FormLabel, FormInput, FormValidationMessage, Card } from 'react-native-elements';
 import { Formik, yupToFormErrors } from 'formik';
 import * as Yup from 'yup';
@@ -11,81 +11,85 @@ import { updateUserData } from '../actions/userAction';
 class SignUp extends React.Component {
     render() {
         return (
-            <View style={{ paddingVertical: 20 }}>
-                <Formik
-                    initialValues={{
-                        name: 'test',
-                        email: 'test@test.com',
-                        password: 'testtest',
-                        passwordConfirm: 'testtest'
-                    }}
-                    onSubmit={values => this.handleSignUp(values)}
-                    validationSchema={Yup.object().shape({
-                        name: Yup
-                            .string()
-                            .min(4, '4文字以上です。')
-                            .required('名前は必須です。'),
-                        email: Yup
-                            .string()
-                            .email('emailの形式ではありません。')
-                            .required('emailは必須です。'),
-                        password: Yup
-                            .string()
-                            .min(4, '4文字以上です。')
-                            .required('パスワードは必須です。'),
-                        passwordConfirm: Yup
-                            .string()
-                            .required('パスワードは必須です。')
-                            .oneOf([Yup.ref('password')], 'パスワードが一致しません。')
-                    })}
-                >
-                    {
-                        ({ handleSubmit, handleChange, values, errors, touched, handleBlur }) => (
-                            <Card title='サインアップ'>
-                                <FormLabel>名前</FormLabel>
-                                <FormInput
-                                    autoCapitalize='none'
-                                    value={values.name}
-                                    onChangeText={handleChange('name')}
-                                    onBlur={handleBlur('name')}
-                                />
-                                {(touched.name && errors.name) && <FormValidationMessage>{errors.name}</FormValidationMessage>}
-                                <FormLabel>Email</FormLabel>
-                                <FormInput
-                                    autoCapitalize='none'
-                                    value={values.email}
-                                    onChangeText={handleChange('email')}
-                                    onBlur={handleBlur('email')}
-                                />
-                                {(touched.email && errors.email) && <FormValidationMessage>{errors.email}</FormValidationMessage>}
-                                <FormLabel>パスワード</FormLabel>
-                                <FormInput
-                                    autoCapitalize='none'
-                                    value={values.password}
-                                    onChangeText={handleChange('password')}
-                                    onBlur={handleBlur('password')}
-                                    secureTextEntry
-                                />
-                                {(touched.password && errors.password) && <FormValidationMessage>{errors.password}</FormValidationMessage>}
-                                <FormLabel>パスワード（確認）</FormLabel>
-                                <FormInput
-                                    autoCapitalize='none'
-                                    value={values.passwordConfirm}
-                                    onChangeText={handleChange('passwordConfirm')}
-                                    onBlur={handleBlur('passwordConfirm')}
-                                    secureTextEntry
-                                />
-                                {(touched.passwordConfirm && errors.passwordConfirm) && <FormValidationMessage>{errors.passwordConfirm}</FormValidationMessage>}
-                                <Button
-                                    title='サインアップ'
-                                    onPress={handleSubmit}
-                                    buttonStyle={{ marginTop: 20 }}
-                                    backgroundColor='#6666FF'
-                                />
-                            </Card>
-                        )
-                    }
-                </Formik>
+            <View style={{ paddingVertical: 20, flex:1 }}>
+                <KeyboardAvoidingView behavior="position">
+                    <ScrollView>
+                        <Formik
+                            initialValues={{
+                                name: 'test',
+                                email: 'test@test.com',
+                                password: 'testtest',
+                                passwordConfirm: 'testtest'
+                            }}
+                            onSubmit={values => this.handleSignUp(values)}
+                            validationSchema={Yup.object().shape({
+                                name: Yup
+                                    .string()
+                                    .min(4, '4文字以上です。')
+                                    .required('名前は必須です。'),
+                                email: Yup
+                                    .string()
+                                    .email('emailの形式ではありません。')
+                                    .required('emailは必須です。'),
+                                password: Yup
+                                    .string()
+                                    .min(4, '4文字以上です。')
+                                    .required('パスワードは必須です。'),
+                                passwordConfirm: Yup
+                                    .string()
+                                    .required('パスワードは必須です。')
+                                    .oneOf([Yup.ref('password')], 'パスワードが一致しません。')
+                            })}
+                        >
+                            {
+                                ({ handleSubmit, handleChange, values, errors, touched, handleBlur }) => (
+                                    <Card title='サインアップ'>
+                                        <FormLabel>名前</FormLabel>
+                                        <FormInput
+                                            autoCapitalize='none'
+                                            value={values.name}
+                                            onChangeText={handleChange('name')}
+                                            onBlur={handleBlur('name')}
+                                        />
+                                        {(touched.name && errors.name) && <FormValidationMessage>{errors.name}</FormValidationMessage>}
+                                        <FormLabel>Email</FormLabel>
+                                        <FormInput
+                                            autoCapitalize='none'
+                                            value={values.email}
+                                            onChangeText={handleChange('email')}
+                                            onBlur={handleBlur('email')}
+                                        />
+                                        {(touched.email && errors.email) && <FormValidationMessage>{errors.email}</FormValidationMessage>}
+                                        <FormLabel>パスワード</FormLabel>
+                                        <FormInput
+                                            autoCapitalize='none'
+                                            value={values.password}
+                                            onChangeText={handleChange('password')}
+                                            onBlur={handleBlur('password')}
+                                            secureTextEntry
+                                        />
+                                        {(touched.password && errors.password) && <FormValidationMessage>{errors.password}</FormValidationMessage>}
+                                        <FormLabel>パスワード（確認）</FormLabel>
+                                        <FormInput
+                                            autoCapitalize='none'
+                                            value={values.passwordConfirm}
+                                            onChangeText={handleChange('passwordConfirm')}
+                                            onBlur={handleBlur('passwordConfirm')}
+                                            secureTextEntry
+                                        />
+                                        {(touched.passwordConfirm && errors.passwordConfirm) && <FormValidationMessage>{errors.passwordConfirm}</FormValidationMessage>}
+                                        <Button
+                                            title='サインアップ'
+                                            onPress={handleSubmit}
+                                            buttonStyle={{ marginTop: 20 }}
+                                            backgroundColor='#6666FF'
+                                        />
+                                    </Card>
+                                )
+                            }
+                        </Formik>
+                    </ScrollView>
+                </KeyboardAvoidingView>
             </View>
         );
     }
